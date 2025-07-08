cdfde88a90abcb672d885c779eba8ab9a7b6f755
Dev-Tanaay <kingspyder03@gmail.com>
2025-07-08 23:45:51 +0530

    Update in middleware

--- middleware.ts
+++ middleware.ts
@@ -2,10 +2,7 @@ import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
 export function middleware(request: NextRequest) {
-  // For default NextAuth session cookie names:
-  const sessionToken =
-    request.cookies.get("authjs.session-token")?.value ||
-    request.cookies.get("authjs.csrf-token")?.value;
+  const sessionToken = request.cookies.get("authjs.session-token")?.value
 
   if (!sessionToken) {
     return NextResponse.redirect(new URL("/login", request.url));
